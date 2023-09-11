ff3b7f47099ceb9e0149772f5f24713f3e9b1208
SteveBinary <60712092+SteveBinary@users.noreply.github.com>
2023-09-11 15:23:07 +0200

    Create index.js

---
+++ 2D-triangle/index.js
@@ -0,0 +1,69 @@
+const canvas = document.getElementById("canvas");
+const gl = canvas.getContext("webgl");
+
+const vertecies = [
+  -0.5, -0.5, 0,
+  0.5, -0.5, 0,
+  -0.5, 0.5, 0,
+];
+
+const indices = [0, 1, 2];
+
+////////// create buffers
+
+const vertexBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertecies), gl.STATIC_DRAW);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+const indexBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+////////// vertex shader
+
+const vertexShaderCode = `
+  attribute vec3 coordinates;
+
+  void main() {
+    gl_Position = vec4(coordinates, 1.0);
+  }
+`;
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, vertexShaderCode);
+gl.compileShader(vertexShader);
+
+////////// fragment shader
+
+const fragmentShaderCode = `
+  void main() {
+    gl_FragColor = vec4(0.0, 0.0, 1.0, 0.5);
+  }
+`;
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShader, fragmentShaderCode);
+gl.compileShader(fragmentShader);
+
+////////// shader program
+
+const shaderProgram = gl.createProgram();
+gl.attachShader(shaderProgram, vertexShader);
+gl.attachShader(shaderProgram, fragmentShader);
+
+gl.linkProgram(shaderProgram);
+gl.useProgram(shaderProgram);
+
+////////// connect the components
+
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+const coordinates = gl.getAttribLocation(shaderProgram, "coordinates");
+gl.vertexAttribPointer(coordinates, 3, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(coordinates);
+
+gl.clearColor(0.1, 0.5, 0.1, 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
