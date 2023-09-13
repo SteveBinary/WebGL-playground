ce8756ccdc669e632eea0d693512c535943be9a6
SteveBinary <SteveBinary@users.noreply.github.com>
2023-09-13 10:41:14 +0200

    Give the cube color and animate it.

--- ThreeJS/index.js
+++ ThreeJS/index.js
@@ -1,20 +1,27 @@
 import * as THREE from "three";
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
-camera.position.set(1, 1, 7);
+camera.position.z = 5;
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const cube = new THREE.Mesh(geometry); // TODO: add material
+// const material = new THREE.MeshBasicMaterial({color: 0xeebb00});
+const material = new THREE.MeshNormalMaterial();
+const cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
 
 function render() {
+    requestAnimationFrame(render);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.z += 0.01;
+
     renderer.render(scene, camera);
 }
 
